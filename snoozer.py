543cd050148697f0a323cf8aa8cce69a3dedb531
Gavin Stringham <42777327+gavinguy2@users.noreply.github.com>
2022-03-28 14:14:38 -0700

    Made not specific to me file locations

--- snoozer.py
+++ snoozer.py
@@ -2,11 +2,11 @@ from pydub import AudioSegment
 from pydub.playback import play
 import random
 def faceTimePartySnoozer():
-    snooz1 = AudioSegment.from_mp3("/home/gavin/Desktop/Master_Folder/python/Snoozer/Sounds_Good.mp3")
-    snooz2 = AudioSegment.from_mp3("/home/gavin/Desktop/Master_Folder/python/Snoozer/I_Understand.mp3")
-    snooz3 = AudioSegment.from_mp3("/home/gavin/Desktop/Master_Folder/python/Snoozer/OK.mp3")
-    snooz4 = AudioSegment.from_mp3("/home/gavin/Desktop/Master_Folder/python/Snoozer/Sure_why_not.mp3")
-    snooz5 = AudioSegment.from_mp3("/home/gavin/Desktop/Master_Folder/python/Snoozer/Thanks_For_Coming.mp3")
+    snooz1 = AudioSegment.from_mp3("Sounds_Good.mp3")
+    snooz2 = AudioSegment.from_mp3("I_Understand.mp3")
+    snooz3 = AudioSegment.from_mp3("OK.mp3")
+    snooz4 = AudioSegment.from_mp3("Sure_why_not.mp3")
+    snooz5 = AudioSegment.from_mp3("Thanks_For_Coming.mp3")
     snoozers = [snooz1, snooz2,snooz3,snooz4,snooz5]
     randNum = random.randint(0, 4)
     play(snoozers[randNum])
